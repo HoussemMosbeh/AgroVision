1cace1ba2c7cc607b60accee411d08485e7f4d2c
khalil <mehmlikhalil@gmail.com>
2026-04-29 12:12:14 +0100

    idk

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './context/AuthContext'
 import DashboardLayout from './components/layout/DashboardLayout'
-import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 import Terrain from './pages/Terrain'
 import YieldPrediction from './pages/YieldPrediction'
 import DiseaseDetection from './pages/DiseaseDetection'
@@ -31,7 +31,7 @@ function App() {
             </ProtectedRoute>
           }
         >
-          <Route index element={<Home />} />
+          <Route index element={<Dashboard />} />
           <Route path="terrain" element={<Terrain />} />
           <Route path="yield" element={<YieldPrediction />} />
           <Route path="disease" element={<DiseaseDetection />} />
